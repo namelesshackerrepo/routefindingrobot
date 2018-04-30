83e7c8028ea89b171abef8678ffea2b41f7a4e27
Seth  Koch <sethkoch@Seths-MacBook-Pro.local>
2018-04-30 12:13:09 -0400

    first push

---
+++ js.js
@@ -0,0 +1,33 @@
+const roads = [
+  "Alice's House-Bob's House",   "Alice's House-Cabin",
+  "Alice's House-Post Office",   "Bob's House-Town Hall",
+  "Daria's House-Ernie's House", "Daria's House-Town Hall",
+  "Ernie's House-Grete's House", "Grete's House-Farm",
+  "Grete's House-Shop",          "Marketplace-Farm",
+  "Marketplace-Post Office",     "Marketplace-Shop",
+  "Marketplace-Town Hall",       "Shop-Town Hall",
+  "Seth's House-Cabin"
+];
+
+function buildGraph(edges) {
+  let graph =  Object.create(null);
+  function addEdge(from, to) {
+    if (graph[from] == null) {
+      graph[from] = [to];
+    } else {
+      graph[from].push(to);
+    }
+  }
+  for (let [from, to] of edges.map(road => road.split("-"))) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
+  return graph;
+}
+
+const roadGraph = buildGraph(roads);
+console.log(roadGraph);
+
+  
+
+
